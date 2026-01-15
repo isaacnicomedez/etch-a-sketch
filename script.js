9b3fe15a4756f798e76f649b98f09f05d296e6ec
Isaac <isaacnicomedez1@gmail.com>
2026-01-15 01:08:20 -0800

    Add working counter and div creator

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ function getCount() {
 
     btn.addEventListener("click", () => {
         count = input.value;
+        createCanvas(count);
     });
 }
+
+function createCanvas(count) {
+    for (let i = 0; i < count; i++) {
+        const container = document.querySelector(".container");
+        const div = document.createElement("div");
+
+        div.classList.add("inner-div");
+        container.appendChild(div);
+    }
+}
+
+getCount();
