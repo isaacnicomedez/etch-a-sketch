5a6f7db3526ee9a66b6f53418eea15908102329f
Isaac <isaacnicomedez1@gmail.com>
2026-01-15 00:59:05 -0800

    Add working input handler

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+
+function getCount() {
+    const input = document.querySelector("input");
+    const btn = document.querySelector("button");
+    let count = 0;
+
+    btn.addEventListener("click", () => {
+        count = input.value;
+    });
+}
