b78c598fe0120ecb889973480f85d75ce33df5f8
Isaac <isaacnicomedez1@gmail.com>
2026-01-15 03:07:12 -0800

    Initialize functioning display and input handler

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-
-function getCount() {
-    const input = document.querySelector("input");
-    const btn = document.querySelector("button");
-    let count = 0;
-
-    btn.addEventListener("click", () => {
-        count = input.value;
-        input.focus();
-        input.value = "";
-        if (count > 100) {
-            return alert("Maximum pixel count: 100!");
-        }
-        createCanvas(count);
-    });
-}
-
-function createCanvas(count) {
-    const container = document.querySelector(".container");
-    container.innerHTML = ""; 
-
-    for (let i = 0; i < count; i++) {
-        const div = document.createElement("div");
-
-        div.classList.add("inner-div");
-        container.appendChild(div);
-        checkHover(div);
-    }
-}
-
-function checkHover(div) {
-    div.addEventListener("mouseenter", () => div.classList.add("entered"));
-    div.addEventListener("mouseleave", () => div.classList.remove("entered"));
-}
-
-getCount();
+const gridBtn = document.querySelector(".grid-btn");
+let count = 0;
+gridBtn.addEventListener("click", () => {
+    count = Number(prompt("Grid: "));
+    count *= count;
+    console.log(count);
+});
