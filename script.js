cd61b631e200f21bffd5767a2d059670a03eebc2
Isaac <isaacnicomedez1@gmail.com>
2026-01-15 06:18:55 -0800

    Initialize HTML template

--- script.js
+++
@@ -1,7 +0,0 @@
-const gridBtn = document.querySelector(".grid-btn");
-let count = 0;
-gridBtn.addEventListener("click", () => {
-    count = Number(prompt("Grid: "));
-    count *= count;
-    console.log(count);
-});
